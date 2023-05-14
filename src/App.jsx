bb08df5db99187bd823eae6884ffdbdd7f282630
Jesus Hernandez <jehersi988@gmail.com>
2023-05-13 19:33:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1> HOLA</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
