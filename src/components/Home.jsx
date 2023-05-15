50a8c6b5304071962324972f030e92f9bcd672c7
Jesus Hernandez <jehersi988@gmail.com>
2023-05-15 12:12:38 -0500

    README

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,12 @@
-import { useDispatch } from 'react-redux';
-import LoginTemplate from '../templates/LoginTemplate';
-import { Button } from 'react-bootstrap';
+import LoginTemplate from '../templates/LoginTemplate'
 import { showToast } from '../features/toast/toastSlice';
 showToast
 
 const Home = () => {
-  const dispatch = useDispatch()
+  
   return (
     <LoginTemplate>
-      <Button variant='dark' onClick={() => dispatch(showToast({ message: 'hola'}))}>HOLA</Button>
-      <h1> HOLA MUNDO </h1>
+      
     </LoginTemplate>
   );
 };
